de7a095376d30c33cd08945f9fab1bcd6f89b968
sumeet <sumeettheracer@gmail.com>
2022-12-05 16:01:38 +0530

    first commit

---
+++ setupRemoteMongo.sh
@@ -0,0 +1,3 @@
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb+srv://crio-qkart:crio-qkart@qkart-node.nb1qpng.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://crio-qkart:crio-qkart@qkart-node.nb1qpng.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
